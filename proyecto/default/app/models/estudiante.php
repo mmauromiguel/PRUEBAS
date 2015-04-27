9dabccbe6ed48c1df64597d62017051e8b194667
mmauromiguel <m.mauromiguel@gmail.com>
2015-04-26 22:45:25 -0430

    Revert "Revert "Revert "25/04/2915"""
    This reverts commit 83c1da418c31bc431f1c775a42952ea9b17105e5.

--- proyecto/default/app/models/estudiante.php
+++ proyecto/default/app/models/estudiante.php
@@ -1,18 +1,17 @@
 <?php
 class Estudiante extends ActiveRecord
 {
-	public $logger = true;
-	
 	public function index($page=1) {
     $estudiante = new Estudiante();
     $this->listEstudiante = $estudiante->getEstudiante ($page);
     
     }
     public function initialize(){
-    	$this->has_many('academia');
-    	$this->belongs_to('academia');
+    	$this->has_many("academia");
+    	 
+    	$this->validates_presence_of("cedula");
+    	
+ }  
 
-    	//**$this->validates_presence_of('cedula'); //
-    }
 }
 ?>
